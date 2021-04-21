ed391f48592ee8f77121ca421dad3d2b288f0913
NULLHE4D <adamlibya17@gmail.com>
2021-04-21 16:52:18 +0200

    added tmux config

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ ln -sfv ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sfv ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -sfv ~/.dotfiles/.vimrc ~/.vimrc
 ln -sfv ~/.dotfiles/proxychains4.conf /etc/proxychains4.conf
+ln -sfv ~/.dotfiles/.tmux.conf ~/.tmux.conf
