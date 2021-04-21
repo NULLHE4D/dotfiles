b2f200339ce40b91f0685fcb29389326c22d8b46
NULLHE4D <adamlibya17@gmail.com>
2021-04-21 10:26:50 +0200

    added proxychains-ng

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -sfv ~/.dotfiles/.profile ~/.profile
 ln -sfv ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sfv ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -sfv ~/.dotfiles/.vimrc ~/.vimrc
+ln -sfv ~/.dotfiles/proxychains4.conf /etc/proxychains4.conf
