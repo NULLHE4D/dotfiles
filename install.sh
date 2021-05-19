144a74a1846652e32663f42f0c7ef189942b4c0c
NULLHE4D <adamlibya17@gmail.com>
2021-05-19 09:41:39 +0200

    added graftcp

--- install.sh
+++ install.sh
@@ -9,5 +9,9 @@ ln -sfv ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -sfv ~/.dotfiles/.vimrc ~/.vimrc
 ln -sfv ~/.dotfiles/proxychains4.conf /etc/proxychains4.conf
 ln -sfv ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfv ~/.dotfiles/graftcp-local.conf /etc/graftcp-local/graftcp-local.conf
+
+echo "restarting services.."
+service graftcp-local restart
 
 echo "done.."
