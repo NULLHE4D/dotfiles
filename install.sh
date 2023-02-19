72612f3e3e854f787f5b891c178bc825343e3837
NULLHE4D <adamshebani@protonmail.com>
2023-02-19 14:29:49 +0000

    cleaned up

--- install.sh
+++ install.sh
@@ -2,25 +2,28 @@
 
 # create symlinks for dotfiles
 echo "creating symlinks.."
-ln -sfv ~/.dotfiles/.bashrc ~/.bashrc
-ln -sfv ~/.dotfiles/.profile ~/.profile
 ln -sfv ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sfv ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -sfv ~/.dotfiles/.vimrc ~/.vimrc
-ln -sfv ~/.dotfiles/proxychains4.conf /etc/proxychains4.conf
+#ln -sfv ~/.dotfiles/proxychains4.conf /etc/proxychains4.conf
 ln -sfv ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -sfv ~/.dotfiles/graftcp-local.conf /etc/graftcp-local/graftcp-local.conf
+#ln -sfv ~/.dotfiles/graftcp-local.conf /etc/graftcp-local/graftcp-local.conf
 
 echo "creating directories.."
 mkdir -p ~/.vim/swp
 mkdir -p ~/.vim/undo
 mkdir -p ~/.vim/autoload
 mkdir -p ~/.vim/plugged
+mkdir -p ~/.config/nvim
 
 echo "copying files.."
 cp -r ~/.dotfiles/.vim/UltiSnips ~/.vim
 
+echo "doing other stuff.."
+cat ~/.dotfiles/.bashrc >> ~/.bashrc
+cat ~/.dotfiles/init.vim >> ~/.config/nvim/init.vim
+
 echo "restarting services.."
-service graftcp-local restart
+#service graftcp-local restart
 
 echo "done.."
